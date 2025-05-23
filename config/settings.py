17e8dbf5b5b1963026ba94ace594e6133b142958
Mohsen <sangsefidimohsen84@gmail.com>
2025-05-23 08:34:41 +0330

    Change setting for deploying on host

--- config/settings.py
+++ config/settings.py
@@ -90,11 +90,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/db',
+        conn_max_age=600
+    )
 }
 
 AUTH_USER_MODEL = 'users.User'
